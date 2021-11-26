4e37c6e4b6ce8e5133a93957b760947f3793f2da
EduardoMuchak <eduardomuchak@gmail.com>
2021-11-26 18:30:13 -0300

    Adiciona exercicios 1, 2 e 3

---
+++ 01-fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-prgramacao/dia-01-javascript-primeiros-passos/exercicio01.js
@@ -0,0 +1,14 @@
+const a = 22;
+const b = 8;
+
+let adição = a + b;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let módulo = a % b;
+
+console.log(adição);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+console.log(módulo);
---
+++ 01-fundamentos/bloco-04-introducao-ao-javascript-e-logica-de-prgramacao/dia-01-javascript-primeiros-passos/exercicio02.js
@@ -0,0 +1,8 @@
+const a = 22;
+const b = 8;
+
+if (a > b){
+    console.log(a)
+}
+else
+    console.log(b)
