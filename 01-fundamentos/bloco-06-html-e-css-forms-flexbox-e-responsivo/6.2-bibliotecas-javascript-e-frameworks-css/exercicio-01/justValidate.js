2c9f8f67939fd1c3a7fe4220e44699caffa0f16f
EduardoMuchak <eduardomuchak@gmail.com>
2022-01-06 15:21:59 -0300

    Adiciona resolução parcial do exercicio do dia

---
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/6.2-bibliotecas-javascript-e-frameworks-css/exercicio-01/justValidate.js
@@ -0,0 +1,27 @@
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Nome é obrigatório',
+    }
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
