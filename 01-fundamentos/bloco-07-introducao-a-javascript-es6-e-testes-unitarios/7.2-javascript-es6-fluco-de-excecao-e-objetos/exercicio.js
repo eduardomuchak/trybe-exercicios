01bce7ed30aee88d7ee49967e529890abdb5d7f5
EduardoMuchak <eduardomuchak@gmail.com>
2022-01-13 19:33:49 -0300

    Adiciona método finally

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2-javascript-es6-fluco-de-excecao-e-objetos/exercicio.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/7.2-javascript-es6-fluco-de-excecao-e-objetos/exercicio.js
@@ -19,9 +19,11 @@ function sum() {
   document.getElementById('value2').value = '';
   } catch (error) {
     document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+  } finally {
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
-    }
+  }
+
 
 }
 window.onload = () => {
