806303cf0df35f435fefe4ec98ff578f4d621b54
Eduardo Muchak <eduardomuchak@gmail.com>
2022-02-01 18:49:04 -0300

    Adiciona exercícios do dia

---
+++ 01-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
@@ -0,0 +1,14 @@
+/* const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  rectangleArea(...rectangles);
+  console.log(rectangle[0] * rectangle[1]);
+}); */
+
+const sum = (...numbers) => numbers.reduce((acumulator, currentValue) => acumulator + currentValue)
+console.log(sum(1, 2,50 , 40, 3, -50));
